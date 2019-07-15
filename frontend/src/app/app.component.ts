fc09e40607a8de313e9fb82d9d9aaaa22fdbaa82
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2019-07-15 11:26:29 +0200

    Add injection token

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import Web3 from 'web3';
+import { WEB3 } from './services/tokens';
+import TruffleContract from 'truffle-contract';
+
+//import LinearStrategyAbi from '../../../build/contracts/LinearStrategy.json';
+import LinearStrategyAbi from './LinearStrategy.json';
+import DutchAuctionAbi from '../../../build/contracts/DutchAuction.json';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(@Inject(WEB3) private web3: Web3) {
+
+
+
+
+    this.web3.eth.defaultAccount = web3.eth.accounts[0];
+
+  }
+
+  ngOnInit() {
+    this.init();
+  }
+
+
+
+
+  async init() {
+
+      console.log(LinearStrategyAbi.contractName);
+
+      const linearStrategy = TruffleContract(LinearStrategyAbi);
+      linearStrategy.setProvider(this.web3.currentProvider);
+
+
+      const instance = linearStrategy.deployed();
+
+
+    //const dutch = TruffleContract(DutchAuctionAbi).new();
+
+
+  }
 }
---
+++ frontend/src/app/services/tokens.ts
@@ -0,0 +1,15 @@
+
+import { InjectionToken } from '@angular/core';
+import Web3 from 'web3';
+
+
+export const WEB3 = new InjectionToken<Web3>('web3Token', {
+    providedIn: 'root',
+    factory: () => {
+        try {
+            return new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('http://localhost:7545'));
+        } catch (err) {
+            throw new Error('Unable to retrieve the injected Ethereum provider from MetaMask or Ganache');
+        }
+    }
+});
