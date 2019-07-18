ee9027762f34beb3eec48e9eafa234109f0dc8e6
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2019-07-18 18:41:41 +0200

    Add vickery detail page

--- frontend/src/app/services/tokens.ts
+++ frontend/src/app/services/tokens.ts
@@ -4,6 +4,7 @@ import { ethers } from 'ethers';
 import AuctionsHouseJson from '../../../../build/contracts/AuctionsHouse.json';
 import DutchAuctionJson from '../../../../build/contracts/DutchAuction.json';
 import LinearStrategyJson from '../../../../build/contracts/LinearStrategy.json';
+import VickeryAuctionJson from '../../../../build/contracts/VickeryAuction.json';
 
 export const RpcProvider = new InjectionToken<ethers.providers.Web3Provider>('web3Token', {
     providedIn: 'root',
@@ -20,22 +21,29 @@ export const RpcProvider = new InjectionToken<ethers.providers.Web3Provider>('we
 
 
 
-export const AuctionHouseFactory = new InjectionToken<ethers.ContractFactory>('web3Token', {
+export const AuctionHouseFactory = new InjectionToken<ethers.ContractFactory>('AuctionHouseFactory', {
     providedIn: 'root',
     factory: () => {
         return new ethers.ContractFactory(AuctionsHouseJson.abi, AuctionsHouseJson.bytecode, inject(RpcProvider).getSigner());
     }
 });
 
-export const DutchAuctionFactory = new InjectionToken<ethers.ContractFactory>('web3Token', {
+export const DutchAuctionFactory = new InjectionToken<ethers.ContractFactory>('DutchAuctionFactory', {
     providedIn: 'root',
     factory: () => {
         return new ethers.ContractFactory(DutchAuctionJson.abi, DutchAuctionJson.bytecode, inject(RpcProvider).getSigner()); }
 });
 
 
-export const LinearStrategyFactory = new InjectionToken<ethers.ContractFactory>('web3Token', {
+export const LinearStrategyFactory = new InjectionToken<ethers.ContractFactory>('LinearStrategyFactory', {
     providedIn: 'root',
     factory: () => {
         return new ethers.ContractFactory(LinearStrategyJson.abi, LinearStrategyJson.bytecode, inject(RpcProvider).getSigner()); }
+});
+
+
+export const VickeryAuctionFactory = new InjectionToken<ethers.ContractFactory>('VickeryAuctionFactory', {
+    providedIn: 'root',
+    factory: () => {
+        return new ethers.ContractFactory(VickeryAuctionJson.abi, VickeryAuctionJson.bytecode, inject(RpcProvider).getSigner()); }
 });
--- frontend/src/app/vickery-detail/vickery-detail.component.ts
+++ frontend/src/app/vickery-detail/vickery-detail.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { VickeryAuction, PhaseType } from '../models/vickery-auction.model';
+import { ethers } from 'ethers';
+import { ActivatedRoute } from '@angular/router';
+import { AccountService } from '../services/account.service';
+import { RpcProvider, VickeryAuctionFactory } from '../services/tokens';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-vickery-detail',
@@ -6,10 +12,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vickery-detail.component.scss']
 })
 export class VickeryDetailComponent implements OnInit {
+  public vickery: VickeryAuction;
+  private contractInstance: ethers.Contract;
 
-  constructor() { }
+  public PhaseType = PhaseType;
 
-  ngOnInit() {
+  public bid: {
+    hash: string,
+    nonce: string,
+    deposit: string
+  } = {} as any;
+
+  constructor(private route: ActivatedRoute,
+    public accountService: AccountService,
+    @Inject(RpcProvider) private provider: ethers.providers.Web3Provider,
+    @Inject(VickeryAuctionFactory) private vickeryAuctionFactory: ethers.ContractFactory,
+    private snackBar: MatSnackBar) {
+  }
+
+  private async fetchAuction() {
+    return new VickeryAuction({
+      state: await this.contractInstance.state(),
+      itemName: await this.contractInstance.itemName(),
+      seller: await this.contractInstance.seller(),
+      owner: await this.contractInstance.owner(),
+      address: this.contractInstance.address
+    });
+  }
+
+
+  async ngOnInit() {
+    const contractAddress = this.route.snapshot.paramMap.get("address");
+
+    try {
+      this.contractInstance = await this.vickeryAuctionFactory.attach(contractAddress).deployed();
+
+      this.vickery = await this.fetchAuction();
+
+    } catch (ex) {
+      console.log(ex)
+    }
+
+
+  }
+
+  private async makeBid() {
+    try {
+
+      let gasLimit = await this.provider.estimateGas(this.contractInstance.makeBid);
+      gasLimit = gasLimit.mul(4);
+
+
+      await this.contractInstance.makeBid(ethers.utils.formatBytes32String(this.bid.hash), {
+        value: ethers.utils.bigNumberify(this.bid.deposit),
+        gasLimit: ethers.utils.bigNumberify(gasLimit)
+      })
+      this.snackBar.open("The bid has been sent", "Ok", { duration: 5000 });
+    } catch (ex) {
+      const msg = ex.message.substr(ex.message.lastIndexOf("revert") + "revert".length);
+      this.snackBar.open(msg || "Cannot send the bid", "Ok", { duration: 5000 });
+    } finally {
+      this.vickery = await this.fetchAuction();
+    }
+  }
+
+  private async withdrawal() {
+    try {
+      await this.contractInstance.withdrawal()
+      this.snackBar.open("Half of the deposit has been transferred back", "Ok", { duration: 5000 });
+    } catch (ex) {
+      const msg = ex.message.substr(ex.message.lastIndexOf("revert") + "revert".length);
+      this.snackBar.open(msg || "Cannot ask withdrawal", "Ok", { duration: 5000 });
+    } finally {
+      this.vickery = await this.fetchAuction();
+    }
+  }
+
+  private async openBid() {
+    try {
+      await this.contractInstance.openBid(ethers.utils.bigNumberify(this.bid.nonce))
+      this.snackBar.open("You opened successfully your bid", "Ok", { duration: 5000 });
+    } catch (ex) {
+      const msg = ex.message.substr(ex.message.lastIndexOf("revert") + "revert".length);
+      this.snackBar.open(msg || "Cannot open your bid", "Ok", { duration: 5000 });
+    } finally {
+      this.vickery = await this.fetchAuction();
+    }
   }
 
 }
