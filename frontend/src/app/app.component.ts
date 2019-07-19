09fc47fd04707957658e8aa627ff294851a3fb44
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2019-07-19 18:55:55 +0200

    Add checks for Metamask detection

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -27,27 +27,32 @@ export class AppComponent {
   ngOnInit() {
     this.init();
     // subscribe to metamask's account change event
-    this._window.ethereum.on('accountsChanged', accounts => {
-      this.ngZone.run(() => {
-        // https://github.com/MetaMask/metamask-extension/issues/5826
-        this.accountService.currentAccount = this._window.web3.toChecksumAddress(accounts[0]);
-        this.changeDetector.detectChanges();
-        this.snackBar.open("The account has been changed", "Ok", { duration: 5000 });
-      });
-    })
+    if (this._window.ethereum) {
+      this._window.ethereum.on('accountsChanged', accounts => {
+        this.ngZone.run(() => {
+          // https://github.com/MetaMask/metamask-extension/issues/5826
+          this.accountService.currentAccount = this._window.web3.toChecksumAddress(accounts[0]);
+          this.changeDetector.detectChanges();
+          this.snackBar.open("The account has been changed", "Ok", { duration: 5000 });
+        });
+      })
+    } else {
+      this.snackBar.open("Please install and enable Metamask", "Ok");
+    }
+
   }
 
   /** Connects to metamask account */
   async init() {
-    const accounts = await this._window.ethereum.enable();
+    if (this._window.ethereum) {
+      const accounts = await this._window.ethereum.enable();
 
-    // https://github.com/MetaMask/metamask-extension/issues/5826
-    this.accountService.currentAccount = this._window.web3.toChecksumAddress(accounts[0]);
+      // https://github.com/MetaMask/metamask-extension/issues/5826
+      this.accountService.currentAccount = this._window.web3.toChecksumAddress(accounts[0]);
 
-    this.accountService.houseCurrentAccount = localStorage.getItem("houseAddress");
-    console.log("Current account => " + this.accountService.currentAccount)
-    console.log("Current house => " + this.accountService.houseCurrentAccount)
-
-
+      this.accountService.houseCurrentAccount = localStorage.getItem("houseAddress");
+      console.log("Current account => " + this.accountService.currentAccount)
+      console.log("Current house => " + this.accountService.houseCurrentAccount)
+    }
   }
 }
--- frontend/src/app/services/tokens.ts
+++ frontend/src/app/services/tokens.ts
@@ -15,7 +15,8 @@ export const RpcProvider = new InjectionToken<ethers.providers.Web3Provider>('we
             return new ethers.providers.Web3Provider(_w.web3.currentProvider);
 
         } catch (err) {
-            throw new Error('Unable to retrieve the injected Ethereum provider from MetaMask');
+            console.error('Unable to retrieve the injected Ethereum provider from MetaMask');
+            return null;
         }
     }
 });
@@ -25,32 +26,66 @@ export const RpcProvider = new InjectionToken<ethers.providers.Web3Provider>('we
 export const AuctionHouseFactory = new InjectionToken<ethers.ContractFactory>('AuctionHouseFactory', {
     providedIn: 'root',
     factory: () => {
-        return new ethers.ContractFactory(AuctionsHouseJson.abi, AuctionsHouseJson.bytecode, inject(RpcProvider).getSigner());
+
+        const provider = inject(RpcProvider);
+
+        if (!provider)
+            return null;
+
+        return new ethers.ContractFactory(AuctionsHouseJson.abi, AuctionsHouseJson.bytecode, provider.getSigner());
     }
 });
 
 export const DutchAuctionFactory = new InjectionToken<ethers.ContractFactory>('DutchAuctionFactory', {
     providedIn: 'root',
     factory: () => {
-        return new ethers.ContractFactory(DutchAuctionJson.abi, DutchAuctionJson.bytecode, inject(RpcProvider).getSigner()); }
+
+        const provider = inject(RpcProvider);
+
+        if (!provider)
+            return null;
+
+        return new ethers.ContractFactory(DutchAuctionJson.abi, DutchAuctionJson.bytecode, provider.getSigner());
+    }
 });
 
 
 export const LinearStrategyFactory = new InjectionToken<ethers.ContractFactory>('LinearStrategyFactory', {
     providedIn: 'root',
     factory: () => {
-        return new ethers.ContractFactory(LinearStrategyJson.abi, LinearStrategyJson.bytecode, inject(RpcProvider).getSigner()); }
+
+        const provider = inject(RpcProvider);
+
+        if (!provider)
+            return null;
+
+        return new ethers.ContractFactory(LinearStrategyJson.abi, LinearStrategyJson.bytecode, provider.getSigner());
+    }
 });
 
 
 export const VickeryAuctionFactory = new InjectionToken<ethers.ContractFactory>('VickeryAuctionFactory', {
     providedIn: 'root',
     factory: () => {
-        return new ethers.ContractFactory(VickeryAuctionJson.abi, VickeryAuctionJson.bytecode, inject(RpcProvider).getSigner()); }
+
+        const provider = inject(RpcProvider);
+
+        if (!provider)
+            return null;
+
+        return new ethers.ContractFactory(VickeryAuctionJson.abi, VickeryAuctionJson.bytecode, provider.getSigner());
+    }
 });
 
 export const AbstractAuctionFactory = new InjectionToken<ethers.ContractFactory>('AbstractAuctionFactory', {
     providedIn: 'root',
     factory: () => {
-        return new ethers.ContractFactory(AbstractAuctionJson.abi, AbstractAuctionJson.bytecode, inject(RpcProvider).getSigner()); }
+
+        const provider = inject(RpcProvider);
+
+        if (!provider)
+            return null;
+
+        return new ethers.ContractFactory(AbstractAuctionJson.abi, AbstractAuctionJson.bytecode, provider.getSigner());
+    }
 });
